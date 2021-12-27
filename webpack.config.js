9f71f47f6e251990ccd2b060f9ec43821ecdc6ca
Erik Onarheim <erik.onarheim@gmail.com>
2021-12-26 23:49:08 -0600

    Add webpack for umd bundle

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: {
+        'excalibur-dev-tools': './dev-tools.ts',
+        'excalibur-dev-tools.min': './dev-tools.ts',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        static: '.',
+    },
+    resolve: {
+        fallback: {
+            fs: false
+        },
+        extensions: [".ts", ".js"],
+    },
+    output: {
+        filename: "[name].js",
+        path: path.join(__dirname, "dist"),
+        library: ["ex", "DevTools"],
+        libraryTarget: "umd"
+    },
+    optimization: {
+        minimize: true,
+    },
+    externals: {
+        "excalibur": {
+            commonjs: "excalibur",
+            commonjs2: "excalibur",
+            amd: "excalibur",
+            root: "ex"
+        }
+    },
+    plugins: [
+        //  new BundleAnalyzerPlugin()
+    ]
+};
